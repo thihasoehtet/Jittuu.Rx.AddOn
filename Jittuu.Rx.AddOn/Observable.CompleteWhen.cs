0ae461b16ca2671471ef419ede56ad2a48e8f19b
Thiha Soe Htet <thihasoehtet.isn@gmail.com>
2014-05-13 17:36:15 +0800

    Add Observable.CompleteWhen

---
+++ Jittuu.Rx.AddOn/Observable.CompleteWhen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Jittuu.Rx.AddOn
+{
+    public static class ObservableCompleteWhen
+    {
+        public static IObservable<T> CompleteWhen<T>(this IObservable<T> source, Func<Exception, bool> predicate)
+        {
+            return source.Catch<T, Exception>(ex =>
+            {
+                if (predicate(ex))
+                {
+                    return Observable.Empty<T>();
+                }
+
+                return Observable.Throw<T>(ex);
+            });
+        }
+    }
+}
