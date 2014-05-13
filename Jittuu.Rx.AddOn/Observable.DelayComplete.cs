408b698519e526d15b4ac66ed5ecab2823e4eccd
Thiha Soe Htet <thihasoehtet.isn@gmail.com>
2014-05-13 17:50:31 +0800

    Add Observable DelayComplete and DelayError

---
+++ Jittuu.Rx.AddOn/Observable.DelayComplete.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
+
+namespace Jittuu.Rx.AddOn
+{
+    public static class ObservableDelayComplete
+    {
+        public static IObservable<T> DelayComplete<T>(this IObservable<T> source, TimeSpan dueTime)
+        {
+            return source.DelayComplete(dueTime, Scheduler.Default);
+        }
+
+        public static IObservable<T> DelayComplete<T>(this IObservable<T> source, Func<TimeSpan> dueTimeSelector)
+        {
+            return source.DelayComplete(dueTimeSelector, Scheduler.Default);
+        }
+
+        public static IObservable<T> DelayComplete<T>(this IObservable<T> source, TimeSpan dueTime, IScheduler scheduler)
+        {
+            return DelayComplete(source, () => dueTime, scheduler);
+        }
+
+        public static IObservable<T> DelayComplete<T>(this IObservable<T> source, Func<TimeSpan> dueTimeSelector, IScheduler scheduler)
+        {
+            //  ys = xs.DelayComplete(dueTime)
+            //
+            //
+            //  xs        -----o------o-----o--|->
+            //                 |      |     |  ---dueTime--| 
+            //                 v      v     v              v
+            //  ys        -----o------o-----o--------------|->
+            //
+
+            //
+            //  xs        -----o------o-----o---X-->
+            //                 |      |     |   |
+            //                 v      v     v   v
+            //  ys        -----o------o-----o---X-->
+            //
+
+            return Observable.Create<T>(observer =>
+            {
+                IDisposable scheduling = null;
+
+                var subscription = source.Subscribe(
+                                            observer.OnNext,
+                                            observer.OnError,
+                                            onCompleted: () => scheduling = scheduler.Schedule(dueTimeSelector(), () => observer.OnCompleted()));
+
+                return new CompositeDisposable(scheduling, subscription);
+            });
+        }
+    }
+}
---
+++ Jittuu.Rx.AddOn/Observable.DelayError.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
+
+namespace Jittuu.Rx.AddOn
+{
+    public static class ObservableDelayError
+    {
+        public static IObservable<T> DelayError<T>(this IObservable<T> source, TimeSpan dueTime)
+        {
+            return source.DelayError(dueTime, Scheduler.Default);
+        }
+
+        public static IObservable<T> DelayError<T>(this IObservable<T> source, TimeSpan dueTime, IScheduler scheduler)
+        {
+            return source.DelayError(() => dueTime, scheduler);
+        }
+
+        public static IObservable<T> DelayError<T>(this IObservable<T> source, Func<TimeSpan> dueTime, IScheduler scheduler)
+        {
+            //  ys = xs.DelayError(dueTime)
+            //
+            //  xs        -----o------o-----o---|-->
+            //                 |      |     |   |
+            //                 v      v     v   v
+            //  ys        -----o------o-----o---|-->
+            //
+
+            //
+            //  xs        -----o------o-----o--X->
+            //                 |      |     |  --duetime-| 
+            //                 v      v     v            v
+            //  ys        -----o------o-----o------------X->
+            //
+
+            return Observable.Create<T>(observer =>
+            {
+                return source.Subscribe(observer.OnNext,
+                                        ex => scheduler.Schedule(dueTime(), () => observer.OnError(ex)),
+                                        observer.OnCompleted);
+            });
+        }
+
+        public static IObservable<T> DelayError<T, TException>(this IObservable<T> source, TimeSpan dueTime, TimeSpan exceptionDueTime) where TException : Exception
+        {
+            return source.DelayError<T, TException>(dueTime, exceptionDueTime, Scheduler.Default);
+        }
+
+        public static IObservable<T> DelayError<T, TException>(this IObservable<T> source, TimeSpan dueTime, Func<TException, TimeSpan> exceptionDueTime) where TException : Exception
+        {
+            return source.DelayError<T, TException>(dueTime, exceptionDueTime, Scheduler.Default);
+        }
+
+        public static IObservable<T> DelayError<T, TException>(this IObservable<T> source, TimeSpan dueTime, Func<TException, TimeSpan> exceptionDueTime, IScheduler scheduler) where TException : Exception
+        {
+            //  ys = xs.DelayError(dueTime, exceptionDueTime)
+            //
+            //  xs        -----o------o-----o---|-->
+            //                 |      |     |   |
+            //                 v      v     v   v
+            //  ys        -----o------o-----o---|-->
+            //
+
+            // X is not TException
+            //
+            //  xs        -----o------o-----o--X->
+            //                 |      |     |  --duetime-| 
+            //                 v      v     v            v
+            //  ys        -----o------o-----o------------X->
+            //
+
+            // X is TException
+            //
+            //  xs        -----o------o-----o--X->
+            //                 |      |     |  --exceptionDueTime--| 
+            //                 v      v     v                      v
+            //  ys        -----o------o-----o----------------------X->
+            //
+
+            return Observable.Create<T>(observer =>
+            {
+                return source.Subscribe(observer.OnNext,
+                                        ex =>
+                                        {
+                                            if (ex is TException)
+                                                scheduler.Schedule(exceptionDueTime((TException)ex), () => observer.OnError(ex));
+                                            else
+                                                scheduler.Schedule(dueTime, () => observer.OnError(ex));
+                                        },
+                                        observer.OnCompleted);
+            });
+        }
+
+        public static IObservable<T> DelayError<T, TException>(this IObservable<T> source, TimeSpan dueTime, TimeSpan exceptionDueTime, IScheduler scheduler) where TException : Exception
+        {
+            return source.DelayError<T, TException>(dueTime, _ => exceptionDueTime, scheduler);
+        }
+
+        public static IObservable<T> DelayError<T, TException1, TException2>(this IObservable<T> source, TimeSpan dueTime, TimeSpan exceptionDueTime, IScheduler scheduler)
+        {
+            //  ys = xs.DelayError(dueTime, exceptionDueTime)
+            //
+            //  xs        -----o------o-----o---|-->
+            //                 |      |     |   |
+            //                 v      v     v   v
+            //  ys        -----o------o-----o---|-->
+            //
+
+            // X is not TException
+            //
+            //  xs        -----o------o-----o--X->
+            //                 |      |     |  --duetime-| 
+            //                 v      v     v            v
+            //  ys        -----o------o-----o------------X->
+            //
+
+            // X is TException
+            //
+            //  xs        -----o------o-----o--X->
+            //                 |      |     |  --exceptionDueTime--| 
+            //                 v      v     v                      v
+            //  ys        -----o------o-----o----------------------X->
+            //
+
+            return Observable.Create<T>(observer =>
+            {
+                return source.Subscribe(observer.OnNext,
+                                        ex =>
+                                        {
+                                            if (ex is TException1)
+                                                scheduler.Schedule(exceptionDueTime, () => observer.OnError(ex));
+                                            else if (ex is TException2)
+                                                scheduler.Schedule(exceptionDueTime, () => observer.OnError(ex));
+                                            else
+                                                scheduler.Schedule(dueTime, () => observer.OnError(ex));
+                                        },
+                                        observer.OnCompleted);
+            });
+        }
+
+    }
+}
