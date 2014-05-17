71c1c5fa36a612e097127a92a280910e7c7e568c
Thiha Soe Htet <thihasoehtet.isn@gmail.com>
2014-05-17 14:47:04 +0800

    Add Task.TimeoutAfter

---
+++ Jittuu.Rx.AddOn/Task.TimeoutAfter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+
+namespace Jittuu.Rx.AddOn
+{
+    public static class TaskTimeoutAfter
+    {
+        public static Task TimeoutAfter(this Task task, TimeSpan timeout)
+        {            
+            return task.ToObservable().Timeout(timeout).ToTask();
+        }
+
+        public static Task<T> TimeoutAfter<T>(this Task<T> task, TimeSpan timeout)
+        {
+            return task.ToObservable().Timeout(timeout).ToTask();
+        }
+
+    }
+}
